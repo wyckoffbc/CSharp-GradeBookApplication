708d87e27175855ea1a6a86d4d3784274bcf7b19
Brian Wyckoff <brian-wy@EMCINS.COM>
2020-08-21 16:01:54 -0500

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,29 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("F");
+            }
+            if (averageGrade >= 80)
+            {
+                return 'A';
+            }
+            else if (averageGrade < 80 && averageGrade >= 60)
+            {
+                return 'B';
+            }
+            else if (averageGrade < 60 && averageGrade >= 40)
+            {
+                return 'C';
+            }
+            else if (averageGrade < 40 && averageGrade >= 20)
+            {
+                return 'D';
+            }
+        }
     }
 }
