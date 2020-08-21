0c55665bfbd6292c71333900b1d0af098c9fb031
Brian Wyckoff <brian-wy@EMCINS.COM>
2020-08-21 16:23:09 -0500

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -36,15 +36,15 @@ namespace GradeBook.GradeBooks
             }
             else return 'F';
         }
-        public override void CalculateStudentStatistics(string name)
+        public override void CalculateStatistics()
         {
             if (Students.Count < 5)
             {
-                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a students overall grade.");
             }
             else
             {
-                base.CalculateStudentStatistics(name);
+                base.CalculateStatistics();
             }
         }
     }
